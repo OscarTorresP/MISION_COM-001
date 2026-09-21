7732791c469888e3d2eadec15e16df61c463fdfd
Oscar Torres <os.torresp@duocuc.cl>
2026-09-21 02:49:08 -0300

    JavaDoc y comentarios para describir metodos

---
+++ src/Estudiante.java
@@ -0,0 +1,37 @@
+/**
+ * Representa un estudiante registrado en el sistema académico.
+ */
+public class Estudiante {
+    String nombre;
+    int edad;
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    /**
+     * Determina si el estudiante aprueba según su nota final.
+     *
+     * @param promedio corresponde al promedio obtenida por el estudiante
+     * @return true si la nota es igual o superior a 4.0
+     */
+    public boolean validaPromedio(float promedio){
+        return promedio >=4;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,14 @@ public class Main {
         double total = 50000;
         double totalFinal = total - (total * descuento);
         System.out.println("El usuario tiene "+edad+"\npor lo que tiene un descuento de "+descuento+"\npor lo que da un total de $"+totalFinal+".");
+        Estudiante estudiante = new Estudiante("Pepe",41);
+        float nota1 = 5;
+        float nota2 =3;
+        float nota3 =7;
+        float nota4 =6;
+        float promedio = (nota1+nota2+ nota3+nota4)/4;
+        if (estudiante.validaPromedio(promedio)){
+            System.out.println("El promedio es: "+promedio+"\n"+estudiante.getNombre()+" es aprobado");
+        }
     }
 }
